89b1ae6330ba27db07aa6ee96cafa76efc3e865f
Christian Ortner <chris.ortner@gmail.com>
2009-04-08 19:09:52 +0200

    some factorial fun in first.hs

--- first.hs
+++ first.hs
@@ -1,11 +1,13 @@
-doubleMe x = x + x
-
-doubleSmallNumber x = if x > 100
-  then x
-  else x*2
-
 factorial x = if x > 0 then product [1..x] else 0
 
-swapStringCase str = [swapCharCase c | c <- str]
+factorial' :: Int -> Int
+factorial' 0 = 1
+factorial' 1 = 1
+factorial' x = x * factorial' (x-1)
 
-swapCharCase c = if c `elem` [a..z] then c else c
+factorial'' :: Int -> Int
+factorial'' x
+      | x < 0 = error "You can't make the factorial or a negative number."
+      | x == 0 || x == 1 = 1
+      | x > 1 = x * factorial'' (x-1)
+      
