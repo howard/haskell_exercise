049835fa0b4971affa6c476f23a448a12ad2a893
Christian Ortner <chris.ortner@gmail.com>
2009-04-08 18:27:40 +0200

    solved euler problem 1

--- euler1.hs
+++ euler1.hs
@@ -3,6 +3,12 @@
 Find the sum of all the multiples of 3 or 5 below 1000.-}
 
 
-variant1 = sum [x | x <- [6..999], (x `mod` 3) == 0] + sum [x | x <- [1..999], (x `mod` 5) == 0]
+variant1 = sum [x | x <- [3..999], (x `mod` 3) == 0] + sum [x | x <- [5..999], (x `mod` 5) == 0] --fail
 
-variant2 = sum [3,6..999] + sum [5,10..995]
+variant2 = sum [3,6..999] + sum [5,10..995] --fail
+
+-- variant1 and variant2 probably fail because multiples of 3 AND 5 are added twice.
+
+variant3 = sum  [x | x <- [3..999], (x `mod` 3) == 0 || (x `mod` 5) == 0] --win
+
+euler1 = variant3 --correct
